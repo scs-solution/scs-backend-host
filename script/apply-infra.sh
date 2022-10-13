331010e418215a99201c40150e7150b7a6caf451
rollrat <rollrat.cse@gmail.com>
2022-10-13 23:18:18 +0900

    infra: add download and upload tfstate.tf

--- script/apply-infra.sh
+++ script/apply-infra.sh
@@ -2,6 +2,9 @@
 privateKey=$1
 desc=$2
 
+# replace exsists terraform
+infraName=$(echo $desc | yq -oj eval . | jq '.name')
+
 # download privateKey
 aws s3api get-object --bucket scs-user-pks --key $privateKey $privateKey
 
@@ -16,9 +19,8 @@ ssh-keygen -f ./../$privateKey -y > $private.pub
 aws s3api get-object --bucket scs-config --key secret.tf secret.tf
 
 # download tfstate.tf
+aws s3api get-object --bucket scs-tfstate --key $infraName tfstate.tf
 
-# replace exsists terraform
-infraName=$(echo $desc | yq -oj eval . | jq '.name')
 for row in $(echo $desc | yq -oj eval . | jq '.instances' | jq -r '.[] | @base64'); do
   _jq() {
    echo ${row} | base64 --decode | jq -r ${1}
@@ -28,27 +30,33 @@ for row in $(echo $desc | yq -oj eval . | jq '.instances' | jq -r '.[] | @base64
   instanceSpec=$(_jq '.instanceSpec')
   instanceType=$(_jq '.instanceType')
 
-  cp instance-template.tf instance-$instanceName.tf
-
   case $instanceType in
     normal)
+      template=$("normal-template.tf")
       ;;
     spot)
-      sed -i "s/%%keyname%%/${privateKey}/g; s/%%instancename%%/${infraName}#${instanceName}/g; s/%%instancespec%%/${instanceSpec}/g" instance-$instanceName.tf
+      template=$("instance-template.tf")
       ;;
     mysql)
+      template=$("rdb-template.tf")
       ;;
     redis)
+      template=$("redis-template.tf")
       ;;
   esac
+
+  cp $template instance-$instanceName.tf
+
+  sed -i "s/%%keyname%%/${privateKey}/g; s/%%instancename%%/${infraName}#${instanceName}/g; s/%%instancespec%%/${instanceSpec}/g" instance-$instanceName.tf
 done
 
 # apply
 terraform apply -auto-approve
 
-# get instance id
-
 # upload tfstate.tf
+aws s3api put-object --bucket scs-tfstate --key $infraName tfstate.tf
+
+# get instance id
 
 # remove template
 cd ..
