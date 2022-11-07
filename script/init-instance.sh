4516be5424c3cad8096faf11c37799df124b6fe7
rollrat <rollrat.cse@gmail.com>
2022-11-07 22:38:03 +0900

    backend:

--- script/init-instance.sh
+++ script/init-instance.sh
@@ -6,9 +6,11 @@ updateKey=$3
 privateIp=$(echo $instance | yq -oj eval . | jq -r '.privateIp')
 initialDesc=$(echo $instance | yq -oj eval . | jq -r '.initialDesc')
 
-imageName=$(echo $initalDesc | yq -oj eval . | jq -r '.imageName')
+imageName=$(echo $initialDesc | yq -oj eval . | jq -r '.imageName')
 initialScript=$(echo $initialDesc | yq -oj eval . | jq -r '.initialScript')
-runScript=$(echo $initalDesc | yq -oj eval . | jq -r '.runScript')
+runScript=$(echo $initialDesc | yq -oj eval . | jq -r '.runScript')
+
+echo $imageName
 
 # download privateKey
 aws s3api get-object --bucket scs-user-pks --key $privateKey.key $privateKey
