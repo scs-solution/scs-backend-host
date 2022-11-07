5701242af61819c5195739056d004b0225927cbe
rollrat <rollrat.cse@gmail.com>
2022-11-07 20:16:16 +0900

    update init-instance

--- script/init-instance.sh
+++ script/init-instance.sh
@@ -2,3 +2,19 @@
 instance=$1
 privateKey=$2
 updateKey=$3
+
+privateIp=$(echo $instance | yq -oj eval . | jq -r '.privateIp')
+initialDesc=$(echo $instance | yq -oj eval . | jq -r '.initialDesc')
+
+imageName=$(echo $initalDesc | yq -oj eval . | jq -r '.imageName')
+initialScript=$(echo $initialDesc | yq -oj eval . | jq -r '.initialScript')
+runScript=$(echo $initalDesc | yq -oj eval . | jq -r '.runScript')
+
+# download privateKey
+aws s3api get-object --bucket scs-user-pks --key $privateKey.key $privateKey
+chmod 700 $privateKey
+
+
+
+
+rm $privateKey
