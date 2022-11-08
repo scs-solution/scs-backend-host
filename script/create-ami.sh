2e225f16e8f970319f0a5b5e69bc8a804d22f04d
roseberr <bsc981111@naver.com>
2022-11-09 02:23:08 +0900

    Update create-ami.sh

--- script/create-ami.sh
+++ script/create-ami.sh
@@ -9,8 +9,6 @@ AMI_ID_BASE="ami-00399ec92321828f5"
 
 # The name of the custom AMI.
 # 랜덤값 생성해서 넣기
-# 중복체크는 하지 않음 
-
 AMI_NAME_TARGET=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
 
 # Region in which creating AMI.
@@ -24,16 +22,117 @@ JSON_EC2="ec2-details.json"
 JSON_AMI="ami-details.json"
 
 
+#Create a Device Mapping
+# 밑에 쓴것대로 device-mapping.json에 써라
+
+cat << EOF > device-mapping.json
+[
+    {
+        "DeviceName": "/dev/sda1",
+        "Ebs": {
+            "VolumeSize": 8,
+            "DeleteOnTermination": false,
+            "Encrypted": false
+        }
+    }
+]
+EOF
+
+
 # generate public key from private key
 ssh-keygen -f ./../$privateKey -y > _$privateKey.pub
 
 #Launch an EC2 Instance
+# 기존 보안그룹이 인바운드 ssh 연결을 허용한다고 가정 
+# 그렇지 않으면 security group을 사용해야 한다.
+
 # aws ec2 run-instances \
 #     --region $REGION \
 #     --image-id $AMI_ID_BASE \
 #     --count 1 \
 #     --block-device-mappings file://device-mapping.json \
 #     --instance-type $TYPE \
-#     --key-name $KEYPAIR \
+#     --key-name _$privateKey.pub \
 #     >$JSON_EC2
 
+
+INSTANCE_ID=$(jq -r '.Instances[0].InstanceId' $JSON_EC2)
+
+#dns 검색 
+#Retrieving Public DNS Name
+
+aws ec2 wait instance-status-ok \
+    --region $REGION \
+    --instance-ids $INSTANCE_ID
+
+# 위에 wait이 끝나면 ec2가 연결될 준비가 끝난것이다.
+
+#ec2의 전체 정보를 저장 
+aws ec2 describe-instances \
+    --region $REGION \
+    --instance-ids $INSTANCE_ID \
+    >$JSON_EC2
+
+EC2_DNS=$(jq -r '.Reservations[0].Instances[0].PublicDnsName' $JSON_EC2)
+
+
+#installing software
+#Next we’ll provision the instance via SSH. The StrictHostKeyChecking option just 
+#prevents SSH from stopping to prompt you for confirmation.
+#python docker 설치
+#다른것 설치하고싶으면 아래 적으면 됨 
+
+ssh -o StrictHostKeyChecking=no ubuntu@$EC2_DNS << EOF
+
+sudo apt-get update -y
+sudo apt-get upgrade python3
+
+sudo apt-get install -y docker.io docker-compose
+EOF
+
+
+#Create the AMI
+
+aws ec2 describe-images \
+    --region $REGION \
+    --filters "Name=name,Values=$AMI_NAME_TARGET" \
+    >$JSON_AMI
+
+#이름 중복검사를 통해 만약 있다면 등록을 취소한다.
+AMI_ID_TARGET=$(jq -r '.Images[0].ImageId' $JSON_AMI)
+
+if [ $AMI_ID_TARGET != "null" ]
+then
+   aws ec2 deregister-image --region $REGION --image-id $AMI_ID_TARGET
+fi
+
+#이제 진짜 ami 생성 
+
+aws ec2 create-image \
+    --region $REGION \
+    --instance-id $INSTANCE_ID \
+    --name "$AMI_NAME_TARGET" \
+    --no-reboot \
+    >$JSON_AMI
+
+#custom ami 생성 완료 
+
+
+#Terminate the EC2 Instance
+#Finally we can terminate the EC2 instance weused to build the AMI.
+
+bash
+aws ec2 terminate-instances --region $REGION --instance-ids $INSTANCE_ID
+
+
+#만들어진 custom ami id :
+
+AMI_ID_TARGET=$(jq -r '.ImageId' $JSON_AMI)
+
+# custom ami를 활용해 ec2만들기
+aws ec2 run-instances \
+--region $REGION \
+--image-id $AMI_ID_TARGET \
+--count 1 \
+--instance-type $desc[2] \
+--key-name $KEYPAIR
