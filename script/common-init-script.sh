654797e1fe1d93845715dc33fe0a573095c5d845
rollrat <rollrat.cse@gmail.com>
2022-11-09 21:39:27 +0900

    script: add common-init-script and update init-instance sciprt

---
+++ script/common-init-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum install -y yum-utils docker
+
+sudo service docker start
+
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:ro \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --volume=/dev/disk/:/dev/disk:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  --privileged \
+  --device=/dev/kmsg \
+  gcr.io/cadvisor/cadvisor:v0.36.0
--- script/init-instance.sh
+++ script/init-instance.sh
@@ -14,7 +14,18 @@ runScript=$(echo $initalDesc | yq -oj eval . | jq -r '.runScript')
 aws s3api get-object --bucket scs-user-pks --key $privateKey.key $privateKey
 chmod 700 $privateKey
 
+echo $initialScript >> client-init-script.sh
+echo $runScript >> client-run-script.sh
 
+scp -i $privateKey ./common-init-script.sh ec2-user@$privateIp:/home/ec2-user/
+scp -i $privateKey ./client-init-script.sh ec2-user@$privateIp:/home/ec2-user/
+scp -i $privateKey ./client-run-script.sh ec2-user@$privateIp:/home/ec2-user/
 
+ssh -i $privateKey ec2-user@$privateIp /home/ec2-user/common-init-script.sh
+ssh -i $privateKey ec2-user@$privateIp /home/ec2-user/client-init-script.sh
+ssh -i $privateKey ec2-user@$privateIp /home/ec2-user/client-run-script.sh
 
 rm $privateKey
+rm common-init-script.sh
+rm client-init-script.sh
+rm client-run-script.sh
