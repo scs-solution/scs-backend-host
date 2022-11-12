59348588b047cb8215c8042c419bb895cb8b77c9
rollrat <rollrat.cse@gmail.com>
2022-11-12 14:20:24 +0900

    Update init-instance.sh

--- script/init-instance.sh
+++ script/init-instance.sh
@@ -19,16 +19,15 @@ chmod 700 $privateKey
 echo $initialScript >> client-init-script.sh
 echo $runScript >> client-run-script.sh
 
-scp -i $privateKey ./common-init-script.sh ec2-user@$privateIp:/home/ec2-user/
-scp -i $privateKey ./client-init-script.sh ec2-user@$privateIp:/home/ec2-user/
-scp -i $privateKey ./client-run-script.sh ec2-user@$privateIp:/home/ec2-user/
+scp -o StrictHostKeyChecking=no -i $privateKey ./script/common-init-script.sh ec2-user@$privateIp:/home/ec2-user/
+scp -o StrictHostKeyChecking=no -i $privateKey ./client-init-script.sh ec2-user@$privateIp:/home/ec2-user/
+scp -o StrictHostKeyChecking=no -i $privateKey ./client-run-script.sh ec2-user@$privateIp:/home/ec2-user/
 
-ssh -i $privateKey ec2-user@$privateIp /home/ec2-user/common-init-script.sh
-ssh -i $privateKey ec2-user@$privateIp /home/ec2-user/client-init-script.sh
-ssh -i $privateKey ec2-user@$privateIp /home/ec2-user/client-run-script.sh
+ssh -o StrictHostKeyChecking=no -i $privateKey ec2-user@$privateIp /home/ec2-user/common-init-script.sh
+ssh -o StrictHostKeyChecking=no -i $privateKey ec2-user@$privateIp /home/ec2-user/client-init-script.sh
+ssh -o StrictHostKeyChecking=no -i $privateKey ec2-user@$privateIp /home/ec2-user/client-run-script.sh
 
 rm $privateKey
-rm common-init-script.sh
 rm client-init-script.sh
 rm client-run-script.sh
 rm $privateKey
