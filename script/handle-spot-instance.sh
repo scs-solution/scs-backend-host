40e26f2896fac9b976b8cdbf3a26187e2f5f1ea0
roseberr <bsc981111@gmail.com>
2022-11-25 15:41:37 +0900

    Update handle-spot-instance.sh

--- script/handle-spot-instance.sh
+++ script/handle-spot-instance.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
-target=$1 #aws instance id
+#target=$1 #aws instance id
+#privateKey=$2 #ssh 접속 할 수 있는 비밀 키 
+#desc=$3 #인프라 description name,endpoint,instances
+#updateKey=$4 #handling 완료했을 떄 backend로 신호보내기
+
+instanceId=$1 # 기존의 종료할 instanceid 
 privateKey=$2 #ssh 접속 할 수 있는 비밀 키 
 desc=$3 #인프라 description name,endpoint,instances
-updateKey=$4 #handling 완료했을 떄 backend로 신호보내기
 
+#기존의 생성된 ami id 
+AMI_ID_TARGET=$(jq -r '.ImageId' $JSON_AMI)
 
-# replace exsists terraform
-infraName=$(echo $desc | yq -oj eval . | jq -r '.name')
+REGION="ap-northeast-2"
 
-# download privateKey
-aws s3api get-object --bucket scs-user-pks --key $privateKey.key $privateKey
-chmod 700 $privateKey
-
-# clone infra template
-git clone https://github.com/scs-solution/scs-infra-template $infraName
-cd $infraName
-
-# generate public key from private key
-ssh-keygen -f ./../$privateKey -y > _$privateKey.pub
-
-# download secret.tf
-aws s3api get-object --bucket scs-config --key secret.tf secret.tf
-
-# download tfstate.tf
-aws s3api get-object --bucket scs-tfstate --key $infraName terraform.tfstate
 
 for row in $(echo $desc | yq -oj eval . | jq '.instances' | jq -r '.[] | @base64'); do
   _jq() {
@@ -40,33 +29,83 @@ for row in $(echo $desc | yq -oj eval . | jq '.instances' | jq -r '.[] | @base64
   sed -i "s/%%keyname%%/_${privateKey}/g; s/%%instancename%%/${infraName}-${instanceName}/g; s/%%instancespec%%/${instanceSpec}/g" instance-$instanceName.tf
 done
 
+
+# ami를 활용해 ec2만들기
+aws ec2 run-instances \
+--region $REGION \
+--image-id $AMI_ID_TARGET \
+--count 1 \
+--instance-type $instanceType \
+--key-name $privateKey
+
+#기존의 ec2의 ebs 뽑기 
+
+
+
+
+
+# replace exsists terraform
+#infraName=$(echo $desc | yq -oj eval . | jq -r '.name')
+
+# download privateKey
+#aws s3api get-object --bucket scs-user-pks --key $privateKey.key $privateKey
+#chmod 700 $privateKey
+
+# clone infra template
+#git clone https://github.com/scs-solution/scs-infra-template $infraName
+#cd $infraName
+
+# generate public key from private key
+#ssh-keygen -f ./../$privateKey -y > _$privateKey.pub
+
+# download secret.tf
+#aws s3api get-object --bucket scs-config --key secret.tf secret.tf
+
+# download tfstate.tf
+#aws s3api get-object --bucket scs-tfstate --key $infraName terraform.tfstate
+
+#for row in $(echo $desc | yq -oj eval . | jq '.instances' | jq -r '.[] | @base64'); do
+##  _jq() {
+ #  echo ${row} | base64 --decode | jq -r ${1}
+#  }
+
+#  instanceName=$(_jq '.name')
+#  instanceSpec=$(_jq '.instanceSpec')
+#  instanceType=$(_jq '.instanceType')
+
+  # normal, spot, rds, redis
+#  cp $instanceType-template.tf instance-$instanceName.tf
+
+##  sed -i "s/%%keyname%%/_${privateKey}/g; s/%%instancename%%/${infraName}-${instanceName}/g; s/%%instancespec%%/${instanceSpec}/g" instance-$instanceName.tf
+#done
+
 # remove templates
-rm *-template.tf
+#rm *-template.tf
 
 # remove provider.tf
-rm provider.tf
+#rm provider.tf
 
 # init
-terraform init
+#terraform init
 
 # apply
-terraform apply -auto-approve
+#terraform apply -auto-approve
 
 # upload tfstate.tf
-aws s3api put-object --bucket scs-tfstate --key $infraName --body terraform.tfstate
+#aws s3api put-object --bucket scs-tfstate --key $infraName --body terraform.tfstate
 
 # get instance id
-echo "aws_spot_instance_request.${infraName}-${instanceName}.public_ip" | terraform console
+#echo "aws_spot_instance_request.${infraName}-${instanceName}.public_ip" | terraform console
 
 # sync with backend database
 
 
 
 # remove template
-rm -rf $infraName
+#rm -rf $infraName
 
 # remove key
-rm $privateKey
+#rm $privateKey
 
 
 
