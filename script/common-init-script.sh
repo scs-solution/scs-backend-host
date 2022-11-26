dfc95a2575791f951bdebaaf594249160e10c71a
rollrat <rollrat.cse@gmail.com>
2022-11-26 14:25:14 +0900

    Update common-init-script.sh

--- script/common-init-script.sh
+++ script/common-init-script.sh
@@ -16,9 +16,6 @@ sudo docker run \
   --device=/dev/kmsg \
   gcr.io/cadvisor/cadvisor:v0.36.0
 
-sudo pip3 install webssh
-sudo nohup wssh --origin='http://www.rollrat.com' &
-
 wget https://github.com/scs-solution/scs-packet-capturer/releases/download/0/scs-packet-capturer
 chmox +x scs-packet-capturer
 sudo nohup ./scs-packet-capturer &
